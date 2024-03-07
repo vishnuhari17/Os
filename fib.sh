05b1d22892af1fb7f8da3531d9aedc91fa5e00e4
Vishnuhari V A <69794938+vishnuhari17@users.noreply.github.com>
2024-03-07 11:47:26 +0530

    Added code

---
+++ fib.sh
@@ -0,0 +1,18 @@
+#Write a program to print the Fibonacci series upto n.
+
+echo "Enter a limit"
+read lim
+a=0
+b=1
+echo "The sequence is "
+echo -n "$a "
+echo -n "$b "
+i=2
+while [ $i -lt $lim ]
+do 
+	c=$((a+b))
+	echo -n "$c "
+	a=$b
+	b=$c
+	i=$((i+1))
+done
